d2f43ef3c320e3b35184b3b367b56b88335a41f3
AbdulSaboor-git <abdulsaboor11524@gmail.com>
2024-07-22 02:06:16 +0500

    fixed header

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -10,47 +10,48 @@ import { getCookie } from "cookies-next";
 import useAuthUser from "@/app/hooks/authUser";
 import UserProfileMenu from "../UserProfileMenu";
 
-const navButtons = ["Home", "About Use", "Shop", "Blog", "Contact Us"];
+const navButtons = ["HOME", "ABOUT US", "SHOP", "BLOG", "CONTACT"];
 export default function Navbar() {
   const [openModal, setOpenModal] = useState(false);
   const [isLogin, setIsLogin] = useState(false);
   return (
-    <div className="flex flex-col justify-center items-center pl-[12px] pr-[12px] gap-4">
-      <div className="flex justify-between items-center w-full max-w-[1200px]">
+    <div className="flex flex-col justify-center items-center px-4 gap-4">
+      <div className="flex justify-between pl-[30px]  items-center w-full max-w-[1200px]">
         <img src="/logo.png" className="w-[80px] h-[80px]" />
         <div className="flex gap-8">
           {navButtons.map((button) => (
-            <button key={button} className="font-[16px] font-semibold">
+            <button key={button} className="font-[13px] text-[17px]">
               {button}
             </button>
           ))}
         </div>
         <UserProfileMenu setOpenModal={setOpenModal} isLogedin={isLogin} />
       </div>
-      <div className="flex justify-between items-center w-full max-w-[1200px]">
+      <div className="flex justify-between gap-16 items-center w-full max-w-[1200px]">
         <button
           style={{ background: COLORS.green }}
-          className={`pl-[6px] pr-[6px] pt-[2px] pb-[2px] text-white w-[124px] h-[40px] rounded`}
+          className={"px-8 py-[2px] text-white w-[208px] h-[40px] rounded"}
         >
           Categories
         </button>
         <Search
-          className="max-w-[500px] search"
+          className=" "
           placeholder="input search text"
           allowClear
           enterButton="Search"
           size="large"
           onSearch={() => {}}
         />
-        <div className="flex flex-col gap-[4px] items-center">
-          <div style={{ color: COLORS.red }} className={`font-semibold`}>
-            Your Cart
+        <div className="flex flex-row justify-between gap-6 items-center w-[450px]">
+          <div className="text-gray-500 text-[20px]">❤ 2</div>
+          <div className="text-gray-500 text-[20px]">🛒 3</div>
+          <div className="flex flex-col gap-[4px] items-center">
+            <div style={{ color: COLORS.red }} className={"font-semibold"}>
+              Your Cart
+            </div>
+            <div style={{ color: COLORS.gray }}>2000 RS</div>
           </div>
-          <div style={{ color: COLORS.gray }}>2000 RS</div>
         </div>
-        {/* <button className="pl-[6px] pr-[6px] pt-[2px] pb-[2px] text-white bg-[#00803e] w-[124px] h-[40px]">
-          Categories
-        </button> */}
       </div>
       <Modal
         open={openModal}
