4aa45f81faf50fd0c7dd5bd577251695832f80dc
anishabid-fandist <100930746+anishabid-fandist@users.noreply.github.com>
2024-12-02 12:51:10 +0500

    sub-cat update apis

--- src/pages/api/sub-categories/index.js
+++ src/pages/api/sub-categories/index.js
@@ -6,8 +6,12 @@ export default async function handler(req, res) {
       return POST(req, res);
     case "GET":
       return GET(req, res);
+    case "PUT":
+      return PUT(req, res);
+    case "PATCH":
+      return PATCH(req, res);
     default:
-      res.setHeader("Allow", ["POST", "GET"]);
+      res.setHeader("Allow", ["POST", "GET", "PUT", "PATCH"]);
       return res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
@@ -36,6 +40,92 @@ async function POST(req, res) {
 
     // Create the new subcategory
     const subCategory = await prisma.subCategory.create({
+      data: {
+        name,
+        slug,
+        url,
+        category: {
+          connect: { id: categoryId },
+        },
+      },
+    });
+
+    return res.status(201).json(subCategory);
+  } catch (error) {
+    console.error("Error creating subcategory:", error);
+    return res
+      .status(500)
+      .json({ message: "Internal server error", error: error.message });
+  }
+}
+// PUT: Upate a new subcategory
+async function PUT(req, res) {
+  const { name, slug, url, categoryId } = req.body;
+
+  if (!name || !slug || !url || !categoryId) {
+    return res
+      .status(400)
+      .json({
+        message: "All fields are required: name, slug, url, categoryId",
+      });
+  }
+
+  try {
+    // Check if the main category exists
+    const category = await prisma.category.findUnique({
+      where: { id: categoryId },
+    });
+
+    if (!category) {
+      return res.status(404).json({ message: "Category not found" });
+    }
+
+    // Update subcategory
+    const subCategory = await prisma.subCategory.update({
+      where: { id: parseInt(categoryId) },
+      data: {
+        name,
+        slug,
+        url,
+        category: {
+          connect: { id: categoryId },
+        },
+      },
+    });
+
+    return res.status(201).json(subCategory);
+  } catch (error) {
+    console.error("Error creating subcategory:", error);
+    return res
+      .status(500)
+      .json({ message: "Internal server error", error: error.message });
+  }
+}
+// PATCH: Update subcategory
+async function PATCH(req, res) {
+  const { name, slug, url, categoryId } = req.body;
+
+  if (!name || !slug || !url || !categoryId) {
+    return res
+      .status(400)
+      .json({
+        message: "All fields are required: name, slug, url, categoryId",
+      });
+  }
+
+  try {
+    // Check if the main category exists
+    const category = await prisma.category.findUnique({
+      where: { id: categoryId },
+    });
+
+    if (!category) {
+      return res.status(404).json({ message: "Category not found" });
+    }
+
+    // Update subcategory
+    const subCategory = await prisma.subCategory.update({
+      where: { id: parseInt(categoryId) },
       data: {
         name,
         slug,
